f7af57fc7294badf2ffe2003ceec0d7d68db5804
Michal Przadka <przadka@gmail.com>
2024-02-28 10:09:17 +0100

    Again

--- setup.py
+++ setup.py
@@ -3,9 +3,15 @@ from setuptools import setup, find_packages
 setup(
     name='granda',
     version='0.1.0',
-    packages=find_packages(),  # This now searches for packages at the root
+    packages=find_packages(),  # This should automatically find and include all packages in your project
+    include_package_data=True,  # This tells setuptools to include any data files specified in MANIFEST.in
     install_requires=[
         'numpy',
         'pandas',
+        # other dependencies...
     ],
+    # Optional: specify package data files explicitly
+    # package_data={
+    #     'your_package': ['data_files/*'],
+    # },
 )
