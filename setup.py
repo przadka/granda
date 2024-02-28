b194613be95e4ddd34bfac7c6e11c2413c782980
Michal Przadka <przadka@gmail.com>
2024-02-28 10:02:49 +0100

    Setup

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ from setuptools import setup, find_packages
 setup(
     name='granda',
     version='0.1.0',
-    packages=find_packages(where='src'),  # This should find your packages
-    package_dir={'': 'src'},  # This tells setuptools where to find packages
+    package_dir={'': 'src'},
+    packages=find_packages(where='src'),
     install_requires=[
         'numpy',
         'pandas',
