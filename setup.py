8a87608152a3a611d937f33b1bc4d7e849ece6f8
Michal Przadka <przadka@gmail.com>
2024-02-28 10:06:17 +0100

    Simplified package structure for troubleshooting

--- setup.py
+++ setup.py
@@ -3,8 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name='granda',
     version='0.1.0',
-    package_dir={'': 'src'},
-    packages=find_packages(where='src'),
+    packages=find_packages(),  # This now searches for packages at the root
     install_requires=[
         'numpy',
         'pandas',
