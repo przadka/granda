0a4f874e87d3467dfe4b535d357eba15026b8de4
Michal Przadka <przadka@gmail.com>
2024-05-07 19:52:16 +0200

    Base strategy is now aware of the model that it uses.

--- base_strategy.py
+++ base_strategy.py
@@ -1,8 +1,8 @@
 from abc import ABC, abstractmethod
-import tiktoken
 
 class BaseStrategy(ABC):
-    def __init__(self, dataset):
+    def __init__(self, model, dataset):
+        self.model = model
         self.dataset = dataset
         assert "UID" in self.dataset.columns, "Dataset does not contain UID column"
         self.in_tokens_count = 0
@@ -15,7 +15,7 @@ class BaseStrategy(ABC):
         Named is in a format of <strategy_name>_result.
         """
         # Example: Convert class name to lowercase and replace underscores with spaces
-        return f"{self.__class__.__name__.lower().replace('_', ' ')}_result"
+        return f"{self.model}_{self.__class__.__name__.lower().replace('_', ' ')}_result"
 
     @abstractmethod
     def process_data(self):
@@ -24,13 +24,3 @@ class BaseStrategy(ABC):
         Returns the processed dataset.
         """
         pass
-
-    def get_tokens_count(self, text, model):
-        """
-        Returns the token count of the given text using the specified model.
-        """
-        #TODO Passing model should be removed, and the interaction with the model should be done using some adapter.
-        #TODO The strategy should not be aware of the implementation model details, when using any model to process the data.
-        
-        encoding = tiktoken.encoding_for_model(model)
-        return len(encoding.encode(text))
--- strategy_manager.py
+++ strategy_manager.py
@@ -12,20 +12,20 @@ class StrategyManager:
         """Get the description of a strategy by name, returning a default message if not found."""
         return self.strategies.get(name, {}).get("description", "No description available.")
 
-    def execute_strategy(self, name: str, dataset: Any) -> Any:
+    def execute_strategy(self, name: str, model: str, dataset: Any) -> Any:
         """Execute a strategy by name on the given dataset, raising an error if the strategy is not found."""
         strategy_info = self.strategies.get(name)
         if not strategy_info:
             raise ValueError(f"Strategy '{name}' not found")
-        strategy = strategy_info['class'](dataset)
+        strategy = strategy_info['class'](model, dataset)
         return strategy.process_data()
 
-    def execute_strategies(self, names: List[str], dataset: Any) -> Dict[str, Any]:
+    def execute_strategies(self, names: List[str], model: str, dataset: Any) -> Dict[str, Any]:
         """Execute multiple strategies by name on the given datase. Chain executions, passing the result of one strategy to the next, if needed. Return a the object return by the last strategy, or an error message if the strategy fails."""
 
         result = dataset
         for name in names:
-            result = self.execute_strategy(name, result)
+            result = self.execute_strategy(name, model, result)
             if isinstance(result, dict) and "error" in result:
                 return result
         return result
